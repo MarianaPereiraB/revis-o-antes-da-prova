9b4095f1dde1bd4d3fee1b6b618ec206135d30bd
mariana pereira <marianapereirab2002@gmail.com>
2025-04-22 14:52:21 -0300

    primeiras atividades do dia

---
+++ atv1.py
@@ -0,0 +1,7 @@
+valor1 = int(input("digite o primeiro valor: "))
+valor2 = int(input("digite o segundo valor: "))
+valor3 = int(input("digite o terceiro terceiro valor: "))
+soma = valor1+valor2
+print(f"a soma de do valor A e B é {soma}")
+if soma<valor3:
+        print(f"a soma do valor A ({valor1}) e valor B ({valor2}) é menor que o valor C ({valor3}.)")
